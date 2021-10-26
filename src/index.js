a3382c24bdc74b232c71a27374dd2008ff78494e
jfadev <jfadev@gmail.com>
2021-10-26 20:35:55 -0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,54 +1,68 @@
 #!/usr/bin/env node
 
-const program = require("commander");
-const packageJson = require("../package.json");
-const Runner = require("../src/Runner");
+const venom = require("venom-bot");
 
-program
-  .version(packageJson.version, "-v, --version")
-  .description(packageJson.description);
-
-program
-  .command("start")
-  .description("Start the Chatbot")
-  .option("-d, --debug", "Enable debug mode", false)
-  .action((args, options) => {
-    const runner = new Runner(args, options);
-    runner.start();
+venom
+  // .create()
+  .create(
+    //session
+    'botSession', //Pass the name of the client you want to start the bot
+    //catchQR
+    (base64Qrimg, asciiQR, attempts, urlCode) => {
+      // console.log('Number of attempts to read the qrcode: ', attempts);
+      // console.log('Terminal qrcode: ', asciiQR);
+      // console.log('base64 image string qrcode: ', base64Qrimg);
+      // console.log('urlCode (data-ref): ', urlCode);
+    },
+    // statusFind
+    (statusSession, session) => {
+      console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken || chatsAvailable || deviceNotConnected || serverWssNotConnected || noOpenBrowser
+      //Create session wss return "serverClose" case server for close
+      console.log('Session name: ', session);
+    },
+    // options
+    {
+      folderNameToken: 'tokens', //folder name when saving tokens
+      mkdirFolderToken: '', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
+      headless: true, // Headless chrome
+      devtools: false, // Open devtools by default
+      useChrome: true, // If false will use Chromium instance
+      debug: false, // Opens a debug session
+      logQR: true, // Logs QR automatically in terminal
+      browserWS: '', // If u want to use browserWSEndpoint
+      browserArgs: [''], //Original parameters  ---Parameters to be added into the chrome browser instance
+      puppeteerOptions: {}, // Will be passed to puppeteer.launch
+      disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
+      disableWelcome: true, // Will disable the welcoming message which appears in the beginning
+      updatesLog: true, // Logs info updates automatically in terminal
+      autoClose: 60000, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
+      createPathFileToken: false //creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
+    }
+  )
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
   });
 
+function start(client) {
+  // console.log('aaaaaaaaaa');
 
-program
-  .command("welcome <name>")
-  .alias("hello")
-  .description("Welcome a person")
-  .option("-d, --debug", "enable debug mode", false)
-  .action((args, options) => {
-    const runner = new Runner(args, options);
-    runner.welcome();
+  // client.onAnyMessage(message => {
+  //   console.log('ddddddddddd', message);
+  // });
+
+  // client.onMessage((message) => {
+  client.onAnyMessage(message => {
+    // console.log('bbbbbbbbbbbbb');
+    if (message.body === "Hi" && message.isGroupMsg === false) {
+      client
+        .sendText(message.from, "Welcome Venom 🕷")
+        .then((result) => {
+          console.log("Result: ", result); //return object success
+        })
+        .catch((erro) => {
+          console.error("Error when sending: ", erro); //return object error
+        });
+    }
   });
-
-program
-  .command("ask")
-  .description("Ask the user")
-  .option("-d, --debug", "enable debug mode", false)
-  .action((args, options) => {
-    const runner = new Runner(args, options);
-    runner.ask();
-  });
-
-program
-  .command("config")
-  .description("Output current config")
-  .option("-d, --debug", "enable debug mode", false)
-  .action((options) => {
-    const runner = new Runner({}, options);
-    runner.outputConfig();
-  });
-
-program.on("command:*", (operands) => {
-  console.error(`error: unknown command '${operands[0]}'\n`);
-  program.help();
-});
-
-program.parse(process.argv);
+}
