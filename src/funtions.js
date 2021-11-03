bef8629332fab590b3052944b68d44c0f4bb2de4
jfadev <jfadev@gmail.com>
2021-11-03 18:37:27 -0200

    Update

--- src/funtions.js
+++ src/funtions.js
@@ -7,18 +7,23 @@ export async function start(client, replies) {
     const reply = replies.find((o) => o.pattern.test(body));
     if (reply && message.isGroupMsg === false) {
       if (reply.parent === parentReply) {
-        console.log("Watch pattern: ", reply.pattern);
+        console.log("Watch: ", reply.pattern);
         parentReply = reply.id;
         if (reply.hasOwnProperty('link')) {
           await client
             .sendLinkPreview(message.from, reply.link, reply.message)
-            .then(result => console.log("Result: ", result))
-            .catch(err => console.error("Error: ", err));
-        } else {
+            .then(result => console.log("Result (sendLinkPreview): ", result.to.remote.user))
+            .catch(err => console.error("Error (sendLinkPreview): ", err));
+        } else if (reply.hasOwnProperty('buttons')) {
+          await client
+            .sendButtons(message.from, '', reply.buttons, reply.message)
+            .then(result => console.log("Result (sendButtons): ", result.to.remote.user))
+            .catch(err => console.error("Error (sendButtons): ", err));
+        } else {  
           await client
             .sendText(message.from, reply.message)
-            .then(result => console.log("Result: ", result))
-            .catch(err => console.error("Error: ", err));
+            .then(result => console.log("Result (sendText): ", result.to.remote.user))
+            .catch(err => console.error("Error (sendText): ", err));
         }
       }
     }
