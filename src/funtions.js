e14259c6bd830cdc6022f7de4b7f5b14df95d7f5
jfadev <jfadev@gmail.com>
2021-11-03 19:03:03 -0200

    Update

--- src/funtions.js
+++ src/funtions.js
@@ -7,22 +7,22 @@ export async function start(client, replies) {
     const reply = replies.find((o) => o.pattern.test(body));
     if (reply && message.isGroupMsg === false) {
       if (reply.parent === parentReply) {
-        console.log("Watch: ", reply.pattern);
+        console.log("Read: ", reply.pattern);
         parentReply = reply.id;
         if (reply.hasOwnProperty('link')) {
           await client
             .sendLinkPreview(message.from, reply.link, reply.message)
-            .then(result => console.log("Result (sendLinkPreview): ", message.from, '>', result.to.remote._serialized, ':', reply.message))
+            .then(result => console.log("Write (sendLinkPreview): ", message.from, '>', result.to.remote._serialized, ':', reply.message))
             .catch(err => console.error("Error (sendLinkPreview): ", err));
         } else if (reply.hasOwnProperty('buttons') && reply.hasOwnProperty('description')) {
           await client
             .sendButtons(message.from, reply.message, reply.buttons, reply.description)
-            .then(result => console.log("Result (sendButtons): ", message.from, '>', result.to.remote._serialized, ':', reply.message))
+            .then(result => console.log("Write (sendButtons): ", message.from, '>', result.to.remote._serialized, ':', reply.message))
             .catch(err => console.error("Error (sendButtons): ", err));
         } else {  
           await client
             .sendText(message.from, reply.message)
-            .then(result => console.log("Result (sendText): ", message.from, '>', result.to.remote._serialized, ':', reply.message))
+            .then(result => console.log("Write (sendText): ", message.from, '>', result.to.remote._serialized, ':', reply.message))
             .catch(err => console.error("Error (sendText): ", err));
         }
       }
