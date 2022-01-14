4bf9fefe2bdc5641830742caf976dc5a6d01fb6f
jfadev <jfadev@gmail.com>
2022-01-14 17:27:41 -0200

    Update

---
+++ doc/examples/conversation1.js
@@ -0,0 +1,62 @@
+import { buttons } from "../helpers.js";
+
+/**
+ * Chatbot conversation flow
+ * Example 1
+ */
+export default [
+  {
+    id: 1,
+    parent: 0,
+    pattern: /hello|hi|howdy|good day|good morning|hey|hi-ya|how are you|how goes it|howdy\-do/,
+    message: "Hello! Thank you for contacting me, I am a Chatbot 🤖 , we will gladly assist you.",
+    description: "Can I help with something?",
+    buttons: buttons([
+      "Website",
+      "Linkedin",
+      "Github",
+      "Donate",
+      "Leave a Message",
+    ]),
+  },
+  {
+    id: 2,
+    parent: 1, // Relation with id: 1
+    pattern: /website/,
+    message: "Visit my website and learn more about me!",
+    link: "https://jordifernandes.com/",
+  },
+  {
+    id: 3,
+    parent: 1, // Relation with id: 1
+    pattern: /linkedin/,
+    message: "Visit my LinkedIn profile!",
+    link: "https://www.linkedin.com/in/jfadev",
+  },
+  {
+    id: 4,
+    parent: 1, // Relation with id: 1
+    pattern: /github/,
+    message: "Check my Github repositories!",
+    link: "https://github.com/jfadev",
+  },
+  {
+    id: 5,
+    parent: 1, // Relation with id: 1
+    pattern: /donate/,
+    message: "A tip is always good!",
+    link: "https://jordifernandes.com/donate/",
+  },
+  {
+    id: 6,
+    parent: 1, // Relation with id: 1
+    pattern: /leave a message/,
+    message: "Write your message, I will contact you as soon as possible!",
+  },
+  {
+    id: 7,
+    parent: 6, // Relation with id: 6
+    pattern: /.*/, // Match with all text
+    message: "Thank you very much, your message will be sent to Jordi! Sincerely the Chatbot 🤖 !",
+  },
+];
